d7ac357cedd1910b88573f22c30f1ebe9a4f32c4
InqDen <inq.den@yande.ru>
2022-06-28 20:04:19 +0500

    added output checking in first test description changed in second test

--- tests/test_battle_tests.py
+++ tests/test_battle_tests.py
@@ -3,48 +3,58 @@ from selene.core import command
 from selene.support.shared import browser
 
 
-def test_form_first():
+def test_registration_form():
     browser.open("automation-practice-form")
 
-#заполняем
+#fill out
     browser.element("#firstName").type("Unknown")
     browser.element("#lastName").type("Unknown")
+
     browser.element("#userEmail").type("dsfgdfg@gmail.com")
+
     browser.all(".custom-radio").element_by(have.exact_text("Other")).click()
+
     browser.element("#userNumber").type("1659865123")
+
     browser.element("#dateOfBirth-wrapper").click()
     browser.element(".react-datepicker__month-select").type("September")
     browser.element(".react-datepicker__year-select").type("2006")
     browser.element("[aria-label= 'Choose Saturday, September 16th, 2006']").click()
+
     browser.element("#uploadPicture").type("D:\Prog\Python\Project\guqa_dz_04_selene\e7c772fd52841b814819c58037df730d.jpeg")
+
     browser.element("#subjectsInput").type("History").press_enter().type("English").press_enter()
+
     browser.element("#currentAddress").type("Russia Ekb. Lenina str. 1919 9191")
+
     browser.all(".custom-checkbox").element_by(have.exact_text("Reading")).click()
+
     browser.element("#state").perform(command.js.scroll_into_view).click()
-    browser.element("#state input").type("NCR").press_enter()
-    browser.element("#city").click()
-    browser.element("#city input").type("Gurgaon").press_enter()
+    browser.element("#state input").type("NCR").press_tab()
+    browser.element("#city input").type("Gurgaon").press_tab()
+
     browser.element('footer')._execute_script('element.style.display = "None"')
     browser.element("#submit").press_enter()
-# не разобрался с проверкой
-  #  browser.all(".table ").should(have.texts(
-  #      "Unknown Unknown",
-  #      "dsfgdfg@gmail.com",
-  #      "Other",
-  #      "1659865123",
-  #      "16 September,2006",
-  #      "History, English",
-  #      "Reading",
-  #      "e7c772fd52841b814819c58037df730d.jpeg",
-  #      "Russia Ekb. Lenina str. 1919 9191",
-  #      "NCR Gurgaon"))
 
+# check
+    browser.all("tbody tr").should(have.texts(
+        'Student Name Unknown Unknown',
+        'Student Email dsfgdfg@gmail.com',
+        'Gender Other',
+        'Mobile 1659865123',
+        'Date of Birth 16 September,2006',
+        'Subjects History, English',
+        'Hobbies Reading',
+        'Picture e7c772fd52841b814819c58037df730d.jpeg',
+        'Address Russia Ekb. Lenina str. 1919 9191',
+        'State and City NCR Gurgaon'
+    ))
 
     browser.element("#closeLargeModal").press_enter()
 
-def test_form_second():
+def test_web_table_form():
     browser.open("webtables")
-# added nem
+# added new record no.4
     browser.element("#addNewRecordButton").press_enter()
     browser.element("#firstName").type("Unknown")
     browser.element("#lastName").type("Unknown")
@@ -54,7 +64,7 @@ def test_form_second():
     browser.element("#department").type("Seven")
     browser.element("#submit").press_enter()
 
-# edit record 2
+# edit record no.2
     browser.element("#edit-record-2").click()
     browser.element("#firstName").clear().type("Mr")
     browser.element("#lastName").clear().type("Anderson")
@@ -64,5 +74,5 @@ def test_form_second():
     browser.element("#department").clear().type("seVen")
     browser.element("#submit").press_enter()
 
-# delete record 3
+# delete record no.3
     browser.element("#delete-record-3").click()
